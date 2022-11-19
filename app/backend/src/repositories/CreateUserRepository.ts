b2f5adf1fd2b1c2610bf7a4841b5abd8d7da4597
angelicapedroso <angelicapedroso05@gmail.com>
2022-11-18 23:57:12 -0300

    feat: implement prisma orm

--- app/backend/src/repositories/CreateUserRepository.ts
+++ app/backend/src/repositories/CreateUserRepository.ts
@@ -1,1 +1,24 @@
-export class createUserRepository {}
+import { IUser } from './../interfaces/userInterface';
+import { PrismaClient } from '@prisma/client';
+import { User } from '../entities/User';
+import { CreateUserRepository } from '../services/CreateUserService';
+
+export class PrismaCreateUserRepository implements CreateUserRepository {
+  private prisma: PrismaClient;
+
+  constructor(prisma = new PrismaClient()) {
+    this.prisma = prisma;
+  }
+
+  async create(user: User): Promise<{ user: IUser }> {
+    const newUser = await this.prisma.users.create({
+      data: {
+        username: user.getUsername(),
+        password: user.getPassword(),
+        accountId: 1,
+      },
+    });
+
+    return { user: newUser };
+  }
+}
