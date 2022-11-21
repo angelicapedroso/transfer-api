6442f1db37bbd999856cf8d59ee122dc0b989d73
angelicapedroso <angelicapedroso05@gmail.com>
2022-11-21 19:06:08 -0300

    feat: implement routes

--- app/backend/src/routes/userRoute.ts
+++ app/backend/src/routes/userRoute.ts
@@ -2,14 +2,20 @@ import Router from 'express';
 import ExpressRoutesAdapter from '../adapters/express/express-routes.adapter';
 import { CreateUserControllerFactory } from '../factories/CreateUserControllerFactory';
 import { GetAccountControllerFactory } from '../factories/GetAccountControllerFactory';
+import { CreateTransactionControllerFactory } from '../factories/CreateTransactionControllerFactory';
 import { AuthMiddleware } from '../middlewares/authMiddleware';
+import { GetUserControllerFactory } from '../factories/GetUserControllerFactory';
 
 const router = Router();
 
 const createUserController = CreateUserControllerFactory.make();
 const getAccountController = GetAccountControllerFactory.make();
+const createTransactionController = CreateTransactionControllerFactory.make();
+const getUserController = GetUserControllerFactory.make();
 
 router.post('/', ExpressRoutesAdapter.adapt(createUserController));
 router.get('/account/:id', AuthMiddleware, ExpressRoutesAdapter.adapt(getAccountController));
+router.post('/transaction', ExpressRoutesAdapter.adapt(createTransactionController));
+router.get('/', ExpressRoutesAdapter.adapt(getUserController));
 
 export default router;
