afa83d035dfbc962e078f85b2771b81a58075ca7
angelicapedroso <angelicapedroso05@gmail.com>
2022-11-23 01:32:44 -0300

    chore: install cors

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRoute from './routes/userRoute';
 import loginRoute from './routes/loginRoute';
+import cors from 'cors';
 
 const app = express();
 
@@ -8,6 +9,8 @@ app.use(express.json());
 
 app.use('/user', userRoute);
 app.use('/login', loginRoute);
+
+app.use(cors());
 
 const PORT = 3333;
 
